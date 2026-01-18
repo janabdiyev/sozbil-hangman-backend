8577d1f5378a8e2655aaf8098bc14fdfe0924b4b
Kakajan <kakajanabdiyev@gmail.com>
2026-01-18 13:44:12 +0300

    Add privacy policy

--- hangman_project/urls.py
+++ hangman_project/urls.py
@@ -2,9 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import FileResponse
+from django.views.generic import View
+import os
+
+class PrivacyPolicyView(View):
+    def get(self, request):
+        file_path = os.path.join(settings.BASE_DIR, 'privacy_policy.html')
+        return FileResponse(open(file_path, 'rb'), content_type='text/html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('privacy-policy/', PrivacyPolicyView.as_view(), name='privacy_policy'),
     path('', include('hangman.urls')),
 ]
 
